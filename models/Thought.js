ab2c74890c48c71c9cc259fe4c383581b007ef6a
Jeremy Huynh <jeahuynh18@gmail.com>
2022-01-23 14:49:01 -0800

    created schema and virtual for ThoughtSchema

--- models/Thought.js
+++ models/Thought.js
@@ -1,1 +1,34 @@
 const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormate(createdAtVal)
+        },
+        reactions: [],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
